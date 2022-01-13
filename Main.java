7cc80cf05dacb94643f732a891cb440dba636be0
QuincyBraxton1 <quincy.braxton@hgs.hiddengeniusproject.org>
2022-01-13 02:28:08 +0000

    Made 2 string and 1 int variables.

---
+++ Main.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+    String name = "Quincy";
+    int age = 14;
+    String job = "geologist";
+
+    System.out.println("My name is " + name + ". I am "+ age + ", and my dream job is to be a " + job + ".");
+  }
+}
